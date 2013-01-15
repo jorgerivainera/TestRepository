064337e31af4e2b9fc40613476506237e83966b7
jorge.g.rivainera <jorge.g.rivainera@.accenture.com>
2013-01-15 12:24:54 -0300

    Fist Commit

---
+++ TestProject/src/TestPackage/TestClass.java
@@ -0,0 +1,15 @@
+package TestPackage;
+
+public class TestClass
+
+{
+
+public static void main(String[] args)
+
+{
+
+System.out.println("Git is fun");
+System.out.println("Argentina the best");
+}
+
+}
