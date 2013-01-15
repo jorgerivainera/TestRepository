81e6ec22a8fd77e57ded63a3e1a1bd4a75f3b66c
jorge.g.rivainera <jorge.g.rivainera@.accenture.com>
2013-01-15 17:10:41 -0300

    third commit

--- TestProject/src/TestPackage/TestClass.java
+++ TestProject/src/TestPackage/TestClass.java
@@ -11,6 +11,7 @@ public static void main(String[] args)
 System.out.println("Git is fun");
 System.out.println("Argentina the best");
 System.out.println("Argentina the best of the world");
+System.out.println("Argentina the best of the Universe");
 }
 
 }
